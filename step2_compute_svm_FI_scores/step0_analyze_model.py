5ba8fa7d11270411a37aeb32964432643391738c
greggj2016 <greggj2016@gmail.com>
2022-07-16 14:51:55 -0400

    step 2 now computes SV and Permuation importance scores solely from test folds as described in the previous commit (i.e. the corresponding commit for step 1)

--- step2_compute_svm_FI_scores/step0_analyze_model.py
+++ step2_compute_svm_FI_scores/step0_analyze_model.py
@@ -13,6 +13,8 @@ parser.add_argument('--file', nargs = 1, type = str, action = "store", dest = "f
 parser.add_argument('--method', nargs = 1, type = str, action = "store", dest = "method")
 args = parser.parse_args()
 file = (args.file)[0]
+test_ind_file = "../step1_compute_nn_FI_scores/test_fold_indices/" 
+test_ind_file += "".join(file.split("/")[2].split("."))[:-3] + ".txt"
 method = (args.method)[0]
 
 note = file.split("/")[2].split(".")
@@ -21,26 +23,15 @@ test_path = "permutation_scores/" + method + "_" + "".join(note) + ".txt"
 if os.path.exists(test_path):
     exit()
 
-def k_fold_CV(data, k, main_col_name, num_layers, alpha):
-    X = data[0]
-    y = data[1]
-    kf = KFold(n_splits = k, shuffle = True)
-    kf.get_n_splits(X)
-    efficacies =  np.array([validate_fold(data, indices, main_col_name, 
-                                          num_layers, alpha)
-                            for indices in kf.split(X)]) 
-    simple_model_effectiveness = efficacies[:, 0, :]
-    neural_network_effectiveness = efficacies[:, 1, :]
-    print(np.mean(simple_model_effectiveness, axis = 0))
-    return([simple_model_effectiveness, neural_network_effectiveness])\
-
 df = pd.read_csv(file, delimiter = "\t", header = 0)
 X = df.loc[:, df.columns[:-1]].to_numpy()
 y = df.loc[:, df.columns[-1]].to_numpy()
 
-kf = KFold(n_splits = 5, shuffle = True)
-kf.get_n_splits(X)
-index_sets = [indices for indices in kf.split(X)]
+test_indices_df = pd.read_csv(test_ind_file, delimiter = "\t")
+all_indices = np.arange(len(y))
+test_indices = [ind for ind in test_indices_df.to_numpy().T]
+train_indices = [np.setdiff1d(all_indices, ind) for ind in test_indices_df.to_numpy().T] 
+index_sets = [inds for inds in zip(train_indices, test_indices)]
 X_train_sets = [X[indices[0]] for indices in index_sets]
 y_train_sets = [y[indices[0]] for indices in index_sets]
 X_test_sets = [X[indices[1]] for indices in index_sets]
@@ -57,9 +48,11 @@ if method == "GB":
     custom_parameter_values = {"max_depth": 5, "tol": 1E-8}
 
 test_accuracy = []
+fitters = []
 for i in range(5):
     fitter = model(**custom_parameter_values)
     fitter.fit(X_train_sets[i], y_train_sets[i])
+    fitters.append(COPY(fitter))
     y_est = fitter.predict_proba(X_test_sets[i])[:, 1]
     test_accuracy.append(pearsonr(y_test_sets[i], y_est)[0])
 
@@ -68,22 +61,26 @@ path = "mean_accuracies/" + method + "_" + "".join(note) + "_" + str(mean_acc) +
 low_acc_file = open(path, "w")
 low_acc_file.close()
 
-permutation_scores = []
-fitter = model(**custom_parameter_values)
-fitter.fit(X, y)
-for i in range(len(X[0])):
-    X_perm = COPY(X)
-    np.random.shuffle(X_perm[:, i])
-    y_est_perm = fitter.predict_proba(X_perm)[:, 1]
-    score = mean_acc**2 - pearsonr(y, y_est_perm)[0]**2
-    permutation_scores.append(score)
+all_permutation_scores = []
+shap_values = np.zeros(X.shape)
+for i, acc, fitter in zip(np.arange(5), test_accuracy, fitters):
 
-fitted_function = lambda x: fitter.predict_proba(x)[:,1]
-reference = np.zeros((100, len(X[0])))
-explainer = shap.KernelExplainer(fitted_function, reference)
-shap_values = explainer.shap_values(X, nsamples = 100)
+    X, y, fold =  X_test_sets[i], y_test_sets[i], test_indices[i]
+    fitted_function = lambda x: fitter.predict_proba(x)[:,1]
+    reference = np.zeros((100, len(X[0])))
+    explainer = shap.KernelExplainer(fitted_function, reference)
+    shap_values[fold] = explainer.shap_values(X, nsamples = 100)
 
-permutation_scores = pd.DataFrame(permutation_scores)
+    permutation_scores = []
+    for i in range(len(X[0])):
+        X_perm = COPY(X)
+        np.random.shuffle(X_perm[:, i])
+        y_est_perm = fitter.predict_proba(X_perm)[:, 1]
+        score = acc**2 - pearsonr(y, y_est_perm)[0]**2
+        permutation_scores.append(score)
+    all_permutation_scores.append(permutation_scores)
+
+permutation_scores = pd.DataFrame(np.mean(all_permutation_scores, axis = 0))
 path = "permutation_scores/" + method + "_" + "".join(note) + ".txt"
 permutation_scores.to_csv(path, sep = "\t", header = False, index = False)
 
